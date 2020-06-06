4058cc007ddd00fa7c87b7bc3e4e48177ccadf35
Andrey Sobol <baskerville@andreysobol.com>
2020-06-07 00:52:39 +0300

    +lv

--- limited_voting.py
+++ limited_voting.py
@@ -1,1 +1,30 @@
-print("hi")
+#players = ("affiliated", "with_preferences", "neutral")
+
+players = (
+    "affiliated1",
+    "affiliated2",
+    "affiliated3",
+    "with_preferences1",
+    "with_preferences2", 
+    "with_preferences3",
+    "neutral",
+)
+
+
+strategies = ("vote1", "vote2", "vote3")
+
+from itertools import product
+
+multiplied_strategies = list(product(
+    strategies,
+    strategies,
+    strategies,
+    strategies,
+    strategies,
+    strategies,
+    strategies,
+))
+
+def calculate_payoffs(multiplied_strategies):
+    r = {}
+    return r
